a9cdc3139f7ca638acde5c0c015c8376f8df3476
SadLaboka <toteshephysic@yandex.ru>
2021-02-01 19:15:12 +0300

    added pages for displaying posts by tag

--- blog/views.py
+++ blog/views.py
@@ -45,4 +45,15 @@ class GetPost(DetailView):
 
 
 class PostsByTag(ListView):
-    pass
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(tags__slug=self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Записи по тегу: ' + str(Tag.objects.get(slug=self.kwargs['slug']))
+        return context
